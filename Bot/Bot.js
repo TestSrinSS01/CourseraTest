7132cd75cc914ba8992940705704b6fecd55d005
Srinjoy <srinjy.s.01.in@gmail.com>
2020-09-24 18:50:03 +0530

    test

---
+++ Bot/Bot.js
@@ -0,0 +1,8 @@
+const Bot = new Discord.Client()
+Bot.on('ready', ()=>{
+    document.write(`${Bot.user.username} logged in!!!`)
+})
+Bot.on('message', msg =>{
+    if (msg.content === 'Hi') msg.channel.send('Hello')
+})
+Bot.login('NzU2MTQxNjk5ODQzMjI3NzAx.X2Nh1Q.qSu49mfvzf8Orlxn3zYqiisH4AA').then(console.log)
