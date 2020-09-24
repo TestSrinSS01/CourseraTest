d3fe83d87f546a72f6752206ec818fb34b30a615
Srinjoy <srinjy.s.01.in@gmail.com>
2020-09-24 19:33:47 +0530

    test

--- Bot/Bot.js
+++ Bot/Bot.js
@@ -1,9 +1,9 @@
-const Bot = new Discord.Client()
-Bot.on('ready', ()=>{
-    document.write(`${Bot.user.username} logged in!!!`)
-})
-Bot.on('message', msg =>{
-    if (msg.content === 'Hi') msg.channel.send('Hello')
-})
-Bot.login('NzU2MTQxNjk5ODQzMjI3NzAx.X2Nh1Q.qSu49mfvzf8Orlxn3zYqiisH4AA').then(console.log)
-//https://testsrinss01.github.io/CourseraTest/Bot/Start.html
+function pat(a) {
+    const b = parseInt(a)
+    let k = 0
+    for (let i = 0; i<b; i++) {
+        for (let j = 0; j < b; j++)
+            document.write(`${String.fromCharCode(k++)}\t`)
+        document.write('<br>')
+    }
+}
