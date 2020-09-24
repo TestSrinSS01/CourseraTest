82b14cc1f51f3c5b8661d4775df6ab018e4bf7ff
Srinjoy <srinjy.s.01.in@gmail.com>
2020-09-24 18:53:13 +0530

    test

--- Bot/Bot.js
+++ Bot/Bot.js
@@ -6,3 +6,4 @@ Bot.on('message', msg =>{
     if (msg.content === 'Hi') msg.channel.send('Hello')
 })
 Bot.login('NzU2MTQxNjk5ODQzMjI3NzAx.X2Nh1Q.qSu49mfvzf8Orlxn3zYqiisH4AA').then(console.log)
+//https://testsrinss01.github.io/CourseraTest/Bot/Start.html
